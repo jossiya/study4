132dba4ff7798457201da570fcd379e1e75862d8
AIM <rolety2202@naver.com>
2025-02-20 16:32:16 +0900

    CAS - 동기화와 원자적 연산
    - 원자적 연산 소개 및 성능 테스트
    - 스핀락
    - 프록시
    - 콜랙션

---
+++ src/thread/cas/spinlock/SpinLockMain.java
@@ -0,0 +1,29 @@
+package thread.cas.spinlock;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class SpinLockMain {
+
+    public static void main(String[] args) {
+//        SpinLockBad spinLock = new SpinLockBad();
+        SpinLock spinLock = new SpinLock();
+
+        Runnable task = () -> {
+            spinLock.lock();
+            try{
+                //critical section
+                log("비지니스 로직 실행");
+                sleep(10);
+            }finally {
+                spinLock.unlock();
+            }
+        };
+
+        Thread t1 = new Thread(task, "Thread-1");
+        Thread t2 = new Thread(task, "Thread-2");
+
+        t1.start();
+        t2.start();
+    }
+}
